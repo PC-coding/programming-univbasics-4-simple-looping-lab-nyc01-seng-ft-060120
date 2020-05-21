1ce3ec04a29b0f6de2e434ac1d1c9fba77213e77
Partap Chauhan <partapchauhan8@gmail.com>
2020-05-21 22:46:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,9 @@
 # Write your methods here
 
-def loop_message_five_times(string)
+def loop_message_five_times(message)
   count = 0
-  while count < (5)
+  while count < 5 do
+    puts message
     count += 1
   end
 end
