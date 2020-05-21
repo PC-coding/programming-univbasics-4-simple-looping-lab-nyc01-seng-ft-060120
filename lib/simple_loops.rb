d0b0f9b6371f5dc5542318b32e5763ed46703908
Partap Chauhan <partapchauhan8@gmail.com>
2020-05-21 22:43:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,9 @@
 # Write your methods here
 
-def loop_message_five_times
-  puts "message"(5)
+counter = 0
+pets = ["Dog", "Cat", "Fish", "Bird", "Hamster"]
+ 
+while pets[counter] do
+  puts pets[counter]
+  counter += 1
 end
